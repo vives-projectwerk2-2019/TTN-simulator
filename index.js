e1527c9f5e68d639eff1ebb8e805b056cc6f0cb6
JelleDBZ <jelle.debuyzere@hotmail.be>
2019-03-14 17:17:35 +0100

    Solving merge conflict

--- index.js
+++ index.js
@@ -3,25 +3,26 @@ var client = mqtt.connect('mqtt://labict.be');
 
 client.on('connect', function () {
     button = {
-        button:1,
+        action:4,
+        movement:5,
         dev_id: "ttn_simulator"
     }
 
     newhardware = {
         id: 1,
-        add_1:10 ,
+        add_1: 10,
         add_2: 20,
         add_3: 30,
         dev_id: "ttn_simulator"
     }
 
     client.publish('TTN', JSON.stringify(newhardware));
-    console.log('Publisher: Initial message Sent');
+    console.log('Publisher: Initial message Sent: ' + JSON.stringify(newhardware));
 
     setInterval(function() {
         
         client.publish('TTN', JSON.stringify(button));
-        console.log('Publisher: Message Sent');
+        console.log('Publisher: Message Sent: ' + JSON.stringify(button));
 
     }, 1000);
 });
