77aa18a75bc9f7cd75042621d109d829a8ea876b
JelleDBZ <jelle.debuyzere@hotmail.be>
2019-04-04 14:57:45 +0200

    Changed integer to strings

--- index.js
+++ index.js
@@ -4,22 +4,23 @@ var client = mqtt.connect('mqtt://' + process.env.BROKER_HOST);
 
 client.on('connect', function () {
     button = {
-        action:"X",
-        movement:"left",
+        action: "x",
+        movement: "left",
         dev_id: "ttn_simulator"
     }
 
     newhardware = {
-        id: 1,
-        add_1: 10,
-        add_2: 20,
-        add_3: 30,
+        id: "1",
+        add_1: "10",
+        add_2: "20",
+        add_3: "30",
         dev_id: "ttn_simulator"
     }
 
+    client.publish('hardware', JSON.stringify(newhardware));
+    console.log('Publisher: Hardware message Sent');   
     setInterval(function() {
-        client.publish('hardware', JSON.stringify(newhardware));
-        console.log('Publisher: Hardware message Sent');   
+     
         client.publish('TTN', JSON.stringify(button));
         console.log('Publisher: Data message Sent');
 
